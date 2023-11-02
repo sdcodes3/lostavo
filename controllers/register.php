ab0d25730bf5f8838f123c00d7a00b4fb9eaca48
Shrut Doshi <shrutvd@gmail.com>
2023-11-02 15:47:32 +0530

    login and register

--- controllers/register.php
+++ controllers/register.php
@@ -3,7 +3,7 @@
 require("../db.php");
 
 if(isset($_POST['submit'])){
-    
+    $errorMsg = "";
     $target_dir = "../uploads/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
@@ -13,35 +13,33 @@ if(isset($_POST['submit'])){
     if(isset($_POST["submit"])) {
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
         if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
-            echo "File is not an image.";
+            $errorMsg = "File is not an image";
             $uploadOk = 0;
         }
     }
 
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
+        $errorMsg = "Sorry, file already exists.";
         $uploadOk = 0;
     }
 
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    }
-    else {
+    if($uploadOk == 1) {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            $errorMsg = "Sorry, there was an error uploading your file.";
         }
+    }
+
+    if($errorMsg != ""){
+        echo $errorMsg;
     }
     
     $uname = $_POST['uname'];
     $umail = $_POST['umail'];
     $upass = $_POST['pass'];
-    $ucity = $_POST['city'];
-    $ustate = $_POST['state'];
     $uimage = htmlspecialchars( basename( $_FILES["fileToUpload"]["name"]));
     
     $sql1 = "SELECT * FROM `user` WHERE email = '".$umail."'";
@@ -50,14 +48,22 @@ if(isset($_POST['submit'])){
         echo "User already registered";
     }
     else{
-        $sql2 = "INSERT INTO `user` VALUES(NULL, '$uname', '$umail', '$upass', '$ucity', '$ustate', '$uimage');";
+        $sql2 = "INSERT INTO `user` VALUES(NULL, '$uname', '$umail', '$upass', '$uimage');";
         $res2 = mysqli_query($con, $sql2);
         if($res2){
-            echo "Registration sucess";
+            if($errorMsg == ""){
+                header("location:../index.html");
+            }
+            else{
+                echo "\nRegistration Success";
+            }
         }
         else{
             echo "Error".mysqli_error($res2);
         }
     }
+}
+else{
+    header("location:../register.php");
 }
 ?>
